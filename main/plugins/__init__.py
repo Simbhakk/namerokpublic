a1a4e580e121aa498fa4a2863997165844d46c09
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2023-06-25 18:55:49 +0530

    Update __init__.py

--- main/plugins/__init__.py
+++ main/plugins/__init__.py
@@ -8,6 +8,6 @@ async def Start_msg(bot: stark , m: Message):
     await bot.send_photo(
     m.chat.id,
     photo="https://telegra.ph/file/db68ef17d999fba44333e.jpg",
-    caption = "**Hi i am All in One Extractor Bot**.\n"
-                            "Press **menu commands** to**to use bot**..\n\n"
-                            "**𝗕𝗼𝘁 𝗢𝘄𝗻𝗲𝗿 : Invisible 😁**")
+    caption = "**Hi , Donate some money to keep this service alive**.\n"
+                            "**And motivate bot maker to repair bot regularly**..\n\n"
+                            "**..**")
