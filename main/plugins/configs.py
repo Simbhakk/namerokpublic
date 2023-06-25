2d7cdf7218f79552e8e0fc3126627d247cabc347
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2023-06-25 17:16:11 +0530

    Update configs.py

--- main/plugins/configs.py
+++ main/plugins/configs.py
@@ -5,7 +5,7 @@ class Config(object):
 	API_ID = int(os.environ.get("API_ID", ''))
 	API_HASH = os.environ.get("API_HASH",'')
 	BOT_TOKEN = os.environ.get("BOT_TOKEN", '')
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", 'Publicsavei')
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", 'Publicsave')
 	
 	DATABASE_URL = os.environ.get("DATABASE_URL",'mongodb+srv://devilbot:Rajbot@cluster0.ldrbggy.mongodb.net/?retryWrites=true&w=majority')
 	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", '-1001661389574'))
